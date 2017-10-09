5051254d262421e27de253794bba195d5da3d550
Sergii_Shust <sergii_shust@gmail.com>
2017-10-09 18:43:29 +0300

    import

--- modules/importer/index.js
+++ modules/importer/index.js
@@ -4,6 +4,40 @@ class Importer {
             console.log(JSON.stringify(filenames));
         });
     }
+
+    import(path) {
+        return new Promise((resolve, reject) => {
+            fs.readFile(path, (err, data) => {
+                if (err) reject(err);
+                resolve(data);
+            });
+        });
+    }
+
+    import(path) {
+        const files = fs.readdirSync(path);
+        const promises = files.map((value) => {
+            new Promise((resolve, reject) => {
+                fs.readFile(path, (err, data) => {
+                    if (err) reject(err);
+                    resolve(data);
+                });
+            })
+        })
+        
+        return Promise.all(promises);
+    }
+
+
+    importSync(path) {
+        const files = fs.readdirSync(path);
+        var results = [];
+        files.map((value) => {
+            results[value] = fs.readFileSync(path + value);
+        });
+
+        return results;
+    }
 }
 
 module.exports = Importer;
